f6312d02752abf668c55d0ae30df9e0c1469556c
marouane1031 <94328198+marouane1031@users.noreply.github.com>
2021-11-16 16:14:40 +0100

    Update King.java

--- src/model/King.java
+++ src/model/King.java
@@ -12,6 +12,17 @@ public class King extends Piece {
 	@Override
 	public boolean isPossible(int x_dest, int y_dest, Vector<Piece> blanche, Vector<Piece> noir) {
 		// TODO Auto-generated method stub
+		System.out.println("Dest => x : " + x_dest + " y : " + y_dest + " => OldPiece : " + this);
+		Piece possiblePiece = blanche.stream().filter(p -> p.getX() == x_dest && p.getY() == y_dest).findFirst().orElse(null);
+		//possiblePiece = noir.stream().filter(p -> p.getX() == x_dest && p.getY() == y_dest).findFirst().orElse(possiblePiece);
+		
+		if(possiblePiece != null) {
+			if(possiblePiece.isBlanche() && this.isBlanche()) return false;
+		}
+		
+		if( x_dest > getX()+1 || x_dest < getX()-1) return false;
+		if(y_dest > getY()+1 || y_dest < getY()-1 ) return false;
+		
 		return true;
 	}
 
